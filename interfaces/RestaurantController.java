ac0b8f973dfb7dbbb0ee6bf0f33cee9a3862a8a8
JANGHEEJIN\wkdgm <wkdgmlwls30@naver.com>
2020-08-25 18:25:50 +0900

    레스토랑 예약 사이트 만들기
    1

---
+++ interfaces/RestaurantController.java
@@ -0,0 +1,32 @@
+package kr.co.fastcampus.eatgo.interfaces;
+
+import kr.co.fastcampus.eatgo.application.RestaurantService;
+import kr.co.fastcampus.eatgo.domain.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController // component의 일종
+public class RestaurantController {
+
+    @Autowired // spring이 알아서 repository를 생성해서 이 안에 넣어줌
+    private RestaurantService restaurantService;
+
+    @GetMapping("/restaurants")
+    public List<Restaurant> list() {
+        List<Restaurant> restaurants = restaurantService.getRestaurants();
+
+        return restaurants;
+    }
+
+    @GetMapping("/restaurants/{id}")
+    public Restaurant detail(@PathVariable("id") Long id) {
+        Restaurant restaurant = restaurantService.getRestaurant(id);
+        // 기본정보 + 메뉴정보
+
+        return restaurant;
+    }
+}
